6d7f0ce1b3fe7970926def796fe29b5f0a6a36ca
uswitch-02 <chunxidubian64@gmail.com>
2023-04-29 13:36:04 +0000

    [finish]応用課題５

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,14 @@
 class SearchesController < ApplicationController
+　before_action :authenticate_user!
+
+
   def  seartch
-    
+    @range = params[:range]
+
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @books = Book.looks(params[:search], params[:word])
+    end
   end
-  endsearch
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -36,4 +36,18 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?","#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_march"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 end
