887884b3598af472fe7253413f2638561fabf1b3
uswitch-02 <chunxidubian64@gmail.com>
2023-05-05 15:08:54 +0000

    [update]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,20 +2,18 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-        flash.now[:notice] = 'コメントを投稿しました'
-        render :post_comments
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    if@comment.save
+      render :create
     else
-      render 'books/show'
+      render 'book/show'
+    end
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    flash.now[:alert] = '投稿を削除しました'
-    @book = Book.find(params[:book_id])
-    render :post_comments
+    @comment = BookComment.find(params[:id]).destroy
+    @comment.destroy
   end
 
   def book_comment_params
